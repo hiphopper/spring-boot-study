3d54c901ae49c43e197a4c8f7100f76b6dc3759f
hiphopper <syberhiphop@gmail.com>
2018-09-19 23:41:08 +0900

    111

--- src/main/java/info/thecodinglive/SpringBootMain.java
+++ src/main/java/info/thecodinglive/SpringBootMain.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootMain {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMain.class, args);
+        System.out.println("gg");
     }
 }
